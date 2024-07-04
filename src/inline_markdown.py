659a7a94ebbd5f0d965d0d4f71db33241e9a6876
ITzJustMe13 <oreosmilesoo@gmail.com>
2024-07-04 18:03:57 +0100

    inline_markdown

---
+++ src/inline_markdown.py
@@ -0,0 +1,24 @@
+from textnode import (
+    TextNode,
+    text_type_text,
+    text_type_bold,
+    text_type_italic,
+    text_type_code,
+)
+
+def split_nodes_delimiter(old_nodes, delimiter, text_type):
+    new_nodes = []
+    for node in old_nodes:
+        if node.text_type != text_type_text:
+            new_nodes.append(node)
+        else:
+            segments = node.text.split(delimiter)
+            if len(segment) % 2 == 0:
+                raise ValueError(f"Unmatched delimiter '{delimiter}' found in text: {node.text}")
+                
+            for i , segment in enumerate(segments):
+                if i % 2 == 0:
+                    new_nodes.append(TextNode(segment,text_type_text))
+                else:
+                    new_nodes.append(TextNode(segment,new_text_type))
+    return new_nodes
--- src/textnode.py
+++ src/textnode.py
@@ -29,16 +29,21 @@ def text_node_to_html_node(text_node):
     if text_node.text_type not in VALID_TEXT_TYPES:
         raise Exception("Invalid Text node text type")
 
-    match text_node.text_type:
-        case text_type_text:
-            return LeafNode(None,text_node.text)
-        case text_type_bold:
-            return LeafNode("b",text_node.text)
-        case text_type_italic:
-            return LeafNode("i",text_node.text)
-        case text_type_code:
-            return LeafNode("code",text_node.text)
-        case text_type_link:
-            return LeafNode("a",text_node.text, props={"href":text_node.url})
-        case text_type_image:
-            return LeafNode("img","",props={"src":text_node.url,"alt":text_node.text})
+    if text_node.text_type == text_type_text:
+        return LeafNode(None, text_node.text)
+    elif text_node.text_type == text_type_bold:
+        return LeafNode("b", text_node.text)
+    elif text_node.text_type == text_type_italic:
+        return LeafNode("i", text_node.text)
+    elif text_node.text_type == text_type_code:
+        return LeafNode("code", text_node.text)
+    elif text_node.text_type == text_type_link:
+        if text_node.url is None:
+            raise Exception("Link type text node must have a URL")
+        return LeafNode("a", text_node.text, props={"href": text_node.url})
+    elif text_node.text_type == text_type_image:
+        if text_node.url is None:
+            raise Exception("Image type text node must have a URL")
+        return LeafNode("img", "", props={"src": text_node.url, "alt": text_node.text})
+    else:
+        raise Exception("Unhandled text type")
