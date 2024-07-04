ceaa955c050860e41cd0056e9794bcb4ca3f1297
ITzJustMe13 <oreosmilesoo@gmail.com>
2024-07-04 15:08:51 +0100

    HtmlNode

--- src/main.py
+++ src/main.py
@@ -1,9 +1,11 @@
 from textnode import TextNode
-
+from htmlnode import HTMLNode
 
 def main():
     dummy = TextNode("This is a text node", "bold","https://www.boot.dev")
-
-    print(dummy)
+    node = HTMLNode(tag="a", value="Click here", props={"href": "https://www.google.com", "target": "_blank"})
+    print(node)
+    print(node.props_to_html())
+    
 
 main()
