4d96adf80ab8b7123eaa8a37c147f02b5862392d
ITzJustMe13 <oreosmilesoo@gmail.com>
2024-07-05 00:05:49 +0100

    text to textnodes

--- src/inline_markdown.py
+++ src/inline_markdown.py
@@ -73,3 +73,12 @@ def split_nodes_link(old_nodes):
             new_nodes.append(TextNode(remaining_text, node.text_type))
     
     return new_nodes
+
+def text_to_textnodes(text):
+    nodes = [TextNode(text, text_type_text)]
+    nodes = split_nodes_delimiter(nodes, "**", text_type_bold)
+    nodes = split_nodes_delimiter(nodes, "*", text_type_italic)
+    nodes = split_nodes_delimiter(nodes, "`", text_type_code)
+    nodes = split_nodes_image(nodes)
+    nodes = split_nodes_link(nodes)
+    return nodes
