23b7fc74b0630b5327e47b4e23e515e00a27509a
ITzJustMe13 <oreosmilesoo@gmail.com>
2024-07-04 19:43:11 +0100

    split nodes link and images

--- src/inline_markdown.py
+++ src/inline_markdown.py
@@ -31,3 +31,45 @@ def extract_markdown_images(text):
 def extract_markdown_links(text):
     matches = re.findall(r"\[(.*?)\]\((.*?)\)",text)
     return matches
+
+def split_nodes_image(old_nodes):
+    new_nodes = []
+    for node in old_nodes:
+        image_tups = extract_markdown_images(node.text)
+        remaining_text = node.text
+
+        for image_tup in image_tups:
+            parts = remaining_text.split(f"![{image_tup[0]}]({image_tup[1]})", 1)
+
+            if parts[0]:
+                new_nodes.append(TextNode(parts[0], node.text_type))
+
+            new_nodes.append(TextNode(image_tup[0],text_type_image,image_tup[1]))
+
+            remaining_text = parts[1]
+        
+        if remaining_text:
+            new_nodes.append(TextNode(remaining_text, node.text_type))
+    
+    return new_nodes
+
+def split_nodes_link(old_nodes):
+    new_nodes = []
+    for node in old_nodes:
+        link_tups = extract_markdown_links(node.text)
+        remaining_text = node.text
+
+        for link_tup in link_tups:
+            parts = remaining_text.split(f"[{link_tup[0]}]({link_tup[1]})", 1)
+
+            if parts[0]:
+                new_nodes.append(TextNode(parts[0], node.text_type))
+
+            new_nodes.append(TextNode(link_tup[0],text_type_link,link_tup[1]))
+
+            remaining_text = parts[1]
+        
+        if remaining_text:
+            new_nodes.append(TextNode(remaining_text, node.text_type))
+    
+    return new_nodes
