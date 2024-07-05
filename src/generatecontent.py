3fa55bd31e090db40139dd5df401af8a0edb9e69
ITzJustMe13 <oreosmilesoo@gmail.com>
2024-07-05 15:48:19 +0100

    generate pages recur

--- src/generatecontent.py
+++ src/generatecontent.py
@@ -1,6 +1,30 @@
 import os
+from pathlib import Path
 from block_markdown import markdown_to_html_node
 
+
+def generate_pages_recursive(dir_path_content, template_path, dest_dir_path):
+    
+    if not os.path.exists(dest_dir_path):
+        print(f"Creating root directory: {dest_dir_path}")
+        os.makedirs(dest_dir_path, exist_ok=True)
+
+    for filename in os.listdir(dir_path_content):
+        from_path = os.path.join(dir_path_content, filename)
+        dest_path = os.path.join(dest_dir_path, filename)
+        
+        print(f"Processing: {from_path} -> {dest_path}")
+
+        if os.path.isfile(from_path) and filename.endswith(".md"):
+            dest_path = Path(dest_path).with_suffix(".html")
+            print(f"Generating file: {from_path} -> {dest_path}")
+            generate_page(from_path, template_path, dest_path)
+        elif os.path.isdir(from_path):
+            print(f"Ensuring sub-directory exists: {dest_path}")
+            if not os.path.exists(dest_path):
+                os.makedirs(dest_path, exist_ok=True)
+            # Recurse into the subdirectory
+            generate_pages_recursive(from_path, template_path, dest_path)
 
 def generate_page(from_path, template_path, dest_path):
     print(f" * {from_path} {template_path} -> {dest_path}")
